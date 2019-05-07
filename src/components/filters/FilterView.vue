1ae8c7bc8a107f5362be37091ac4c27a97d4b5b5
Alisa Ono <alisao.mit@gmail.com>
2019-05-07 19:59:36 -0400

    Change filter options to popover

--- src/components/filters/FilterView.vue
+++ src/components/filters/FilterView.vue
@@ -8,12 +8,112 @@
             @text-change="onTextChange">
         </search-bar>
       </div>
-      <button
-          class="toggle-filters"
-          :style="toggleFiltersStyle"
-          @click="toggleFilters">
-        {{ toggleFiltersLabel }}
-      </button>
+      <v-popover
+          class="overflow-menu"
+          popoverClass="filter-options-wrapper"
+          container="#nb-app-wrapper"
+          :show="filterVisible"
+          @hide="onFilterHide">
+        <button
+            class="tooltip-target toggle-filters"
+            :style="toggleFiltersStyle"
+            @click="toggleFilters">
+          {{ toggleFiltersLabel }}
+        </button>
+        <template slot="popover">
+          <div class="nb-tooltip filter-options">
+            <div class="title">Hashtags</div>
+            <div class="hashtags">
+              <div v-for="hashtag in hashtags">
+                <input
+                    v-model="filterHashtags"
+                    type="checkbox"
+                    :id="'filter-hashtag-' + hashtag.id"
+                    :value="hashtag.id"
+                    @change="onFilterChange('hashtags')">
+                <label :for="'filter-hashtag-' + hashtag.id">
+                  {{ hashtag.value }}
+                </label>
+              </div>
+            </div>
+            <div class="title">Comments and Replies</div>
+            <div class="comments-replies">
+              <div>
+                <input
+                    type="checkbox"
+                    id="instructor-comments"
+                    value="instructor"
+                    v-model="filterComments"
+                    @change="onFilterChange('comments')">
+                <label for="instructor-comments">
+                  instructors
+                </label>
+              </div>
+              <div>
+                <input
+                    type="checkbox"
+                    id="my-comments"
+                    value="me"
+                    v-model="filterComments"
+                    @change="onFilterChange('comments')">
+                <label for="my-comments">
+                  me
+                </label>
+              </div>
+            </div>
+            <div class="title">Reply Requests</div>
+            <div class="reply-requests">
+              <div>
+                <input
+                    type="checkbox"
+                    id="anyone-reply-reqs"
+                    value="anyone"
+                    v-model="filterReplyReqs"
+                    @change="onFilterChange('reply-reqs')">
+                <label for="anyone-reply-reqs">
+                  anyone
+                </label>
+              </div>
+              <div>
+                <input
+                    type="checkbox"
+                    id="my-reply-reqs"
+                    value="me"
+                    v-model="filterReplyReqs"
+                    @change="onFilterChange('reply-reqs')">
+                <label for="my-reply-reqs">
+                  me
+                </label>
+              </div>
+            </div>
+            <div class="title">Stars</div>
+            <div class="stars">
+              <div>
+                <input
+                    type="checkbox"
+                    id="anyone-stars"
+                    value="anyone"
+                    v-model="filterStars"
+                    @change="onFilterChange('stars')">
+                <label for="anyone-stars">
+                  anyone
+                </label>
+              </div>
+              <div>
+                <input
+                    type="checkbox"
+                    id="my-stars"
+                    value="me"
+                    v-model="filterStars"
+                    @change="onFilterChange('stars')">
+                <label for="my-stars">
+                  me
+                </label>
+              </div>
+            </div>
+          </div>
+        </template>
+      </v-popover>
       <span
           class="filter-bookmarks"
           v-tooltip="filterBookmarks ? 'clear filter' : 'show bookmarked'"
@@ -24,97 +124,6 @@
         <font-awesome-icon v-else :icon="['far', 'bookmark']" class="icon far">
         </font-awesome-icon>
       </span>
-    </div>
-    <div class="filter-options" v-show="filterVisible">
-      <div class="title">Hashtags</div>
-      <div class="hashtags">
-        <div v-for="hashtag in hashtags">
-          <input
-              v-model="filterHashtags"
-              type="checkbox"
-              :id="'filter-hashtag-' + hashtag.id"
-              :value="hashtag.id"
-              @change="onFilterChange('hashtags')">
-          <label :for="'filter-hashtag-' + hashtag.id">
-            {{ hashtag.value }}
-          </label>
-        </div>
-      </div>
-      <div class="title">Comments and Replies</div>
-      <div class="comments-replies">
-        <div>
-          <input
-              type="checkbox"
-              id="instructor-comments"
-              value="instructor"
-              v-model="filterComments"
-              @change="onFilterChange('comments')">
-          <label for="instructor-comments">
-            by instructors
-          </label>
-        </div>
-        <div>
-          <input
-              type="checkbox"
-              id="my-comments"
-              value="me"
-              v-model="filterComments"
-              @change="onFilterChange('comments')">
-          <label for="my-comments">
-            by me
-          </label>
-        </div>
-      </div>
-      <div class="title">Reply Requests</div>
-      <div class="reply-requests">
-        <div>
-          <input
-              type="checkbox"
-              id="anyone-reply-reqs"
-              value="anyone"
-              v-model="filterReplyReqs"
-              @change="onFilterChange('reply-reqs')">
-          <label for="anyone-reply-reqs">
-            by anyone
-          </label>
-        </div>
-        <div>
-          <input
-              type="checkbox"
-              id="my-reply-reqs"
-              value="me"
-              v-model="filterReplyReqs"
-              @change="onFilterChange('reply-reqs')">
-          <label for="my-reply-reqs">
-            by me
-          </label>
-        </div>
-      </div>
-      <div class="title">Stars</div>
-      <div class="stars">
-        <div>
-          <input
-              type="checkbox"
-              id="anyone-stars"
-              value="anyone"
-              v-model="filterStars"
-              @change="onFilterChange('stars')">
-          <label for="anyone-stars">
-            by anyone
-          </label>
-        </div>
-        <div>
-          <input
-              type="checkbox"
-              id="my-stars"
-              value="me"
-              v-model="filterStars"
-              @change="onFilterChange('stars')">
-          <label for="my-stars">
-            by me
-          </label>
-        </div>
-      </div>
     </div>
   </div>
 </template>
@@ -158,6 +167,9 @@
       },
       toggleFilters: function(event) {
         this.filterVisible = !this.filterVisible
+      },
+      onFilterHide: function() {
+        this.filterVisible = false
       },
       onFilterChange: function(type) {
         switch (type) {
