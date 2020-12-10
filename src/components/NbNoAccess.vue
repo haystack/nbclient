f4d0e4ccf73662e8b9d13cdd7e13f919d6b21564
Jumana <jumana.almahmoud@gmail.com>
2020-12-09 19:18:05 -0500

    detected a user who's not enrolled in a class and showed error msg

---
+++ src/components/NbNoAccess.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="nb-sidebar" class="nb-sidebar">
+        <nav-bar :me="user" @logout="$emit('logout')"></nav-bar>
+        <h1 class="nb-no-access-msg">You don't have access to this source</h1>
+    </div>
+</template>
+<script>
+import NavBar from './NavBar.vue'
+
+export default {
+  name: 'nb-no-access',
+  props: {
+     user: {
+      type: Object,
+      default: () => {}
+    }
+  },
+   components: {
+    NavBar
+  }
+}
+
+</script>
