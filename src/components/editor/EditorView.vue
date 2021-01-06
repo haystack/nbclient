5f02e9f6efec06f56cac2fbaafd63c7d6f5c06f2
Jumana <jumana.almahmoud@gmail.com>
2021-01-06 01:31:22 -0500

    emoji icon and text editor enhancements

--- src/components/editor/EditorView.vue
+++ src/components/editor/EditorView.vue
@@ -120,10 +120,9 @@ export default {
         [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
         [ 'bold', 'italic', 'underline', 'strike' ],
         [{ 'script': 'super' }, { 'script': 'sub' }],
-        [ 'blockquote', 'code-block', 'link', 'formula' ],
+        [ 'blockquote', 'code-block', 'link'/*, 'formula'*/ ],
         [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
         [{ 'align': [] }],
-        [ 'clean' ],
         [ 'hashtag', 'people' ]
       ],
       placeholder: 'Type # or @ to include tags',
