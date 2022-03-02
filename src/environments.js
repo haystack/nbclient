579809a4f4afaa88ede0cdddd6511e5a94801019
Jumana Almahmoud <jumana.almahmoud@gmail.com>
2022-03-02 01:04:14 -0500

    Update environments.js

--- src/environments.js
+++ src/environments.js
@@ -2,7 +2,7 @@ export const Environments = {
     prod: { baseURL: "https://nb2.csail.mit.edu", pluginURL: "https://nb2.csail.mit.edu/client" },
     feature: { baseURL: "https://feature.nb2.csail.mit.edu", pluginURL: "https://feature.nb2.csail.mit.edu/client" },
     dev: { baseURL: "https://127.0.0.1:3000", pluginURL: "https://127.0.0.1:3001" },
-    testJumana: { baseURL: "https://jumana-nb.csail.mit.edu", pluginURL: "https://jumana-nb.csail.mit.edu/client" },
+    test: { baseURL: "https://test.nb2.csail.mit.edu", pluginURL: "https://test.nb2.csail.mit.edu/client" },
     testHelen: { baseURL: "https://helen-nb.csail.mit.edu", pluginURL: "https://helen-nb.csail.mit.edu/client" },
     ngrok: { baseURL:"https://nb2.ngrok.io", pluginURL: "https://nb2.ngrok.io/client"}
 }
